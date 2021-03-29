4bd6eda6f99ef8d50853225cea799c1c8394da36
Tedros2019 <tedrosrghebru@gmail.com>
2021-03-29 02:11:28 -0500

    Adding newProduct form

---
+++ js/newProduct.js
@@ -0,0 +1,20 @@
+
+ function myFunction(){
+   var result1    = document.getElementById("prod").value;
+   var result2   = document.getElementById("name").value;
+   var result3  = document.getElementById("price").value;
+   var result4   = document.getElementById("quantity").value;
+   console.log(result4);
+   var result5   = document.getElementById("sup").value;
+   var result6  = document.getElementById("d").value;
+               
+               document.getElementById("a").innerHTML=result1;
+               document.getElementById("b").innerHTML=result2;
+               document.getElementById("c").innerHTML=result3;
+               document.getElementById("q").innerHTML=result4;
+               document.getElementById("e").innerHTML=result5;
+               document.getElementById("f").innerHTML=result6;
+ 
+   console.log("Hello Tedy");
+ }
+ 
